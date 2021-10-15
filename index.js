38639d0b6602b44771692c1613e69f46378ad62b
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-14 19:43:21 -0700

    commit tree 6b5a2a12ff6c8af9ff8b2b3b766aeb88453e35e2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function  writeCards (names, event){
+    //console.log(names);
+    const newArr = [];
+    for (let aName of names){
+        //console.log(aName);
+        const message = `Thank you, ${aName}, for the wonderful ${event} gift!`;
+        //console.log(message);
+        newArr.push(message);
+    }
+    console.log(newArr);
+    return newArr;
+}
+//writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+function countDown(num){
+    for (let i = num; i>-1; i--){
+        console.log(i);
+    }
+}
